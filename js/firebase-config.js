4c06da5dfbaa3d03dde1db1ba68e197b2386cb48
jojoadventure-create <jojoadventure582@gmail.com>
2026-09-07 13:57:28 +0200

    up

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -12,17 +12,17 @@
    =========================================================== */
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDeEUCBIOj__0Z6hRGmMgT9P5Rb81KDlg8",
-  authDomain: "nuruxdata-1.firebaseapp.com",
-  projectId: "nuruxdata-1",
-  storageBucket: "nuruxdata-1.firebasestorage.app",
-  messagingSenderId: "74795399735",
-  appId: "1:74795399735:web:6b31295dc60b5d63845db2",
-  measurementId: "G-D5Z1CPTSHN"
+  apiKey: "AIzaSyDnS5-jODKNWmWSeQ0ZjRLb0ShospzCzf0",
+  authDomain: "ombelles-school.firebaseapp.com",
+  projectId: "ombelles-school",
+  storageBucket: "ombelles-school.firebasestorage.app",
+  messagingSenderId: "984187931623",
+  appId: "1:984187931623:web:3b477dc147c5b848104b42",
+  measurementId: "G-C7X5SJMD48"
 };
 // Le code établissement DOIT être identique à celui généré dans
 // l'application (Paramètres école → Site web des parents → Générer).
-const SCHOOL_CODE = "ECOLE-IAEM5C";
+const SCHOOL_CODE = "ECOLE-QHULQF";
 
 // Nom affiché en haut du site (peut être différent de la config Firebase)
 const SCHOOL_NAME = "Les ombelles";
